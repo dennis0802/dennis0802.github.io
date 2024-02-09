3478a8ab61a47bcba1e83f605d457a22df22fe15
Dennis <dennisdao2001@hotmail.com>
2024-02-09 09:37:21 -0500

    Dark Mode Verified dark mode was working properly via external XAMPP web server (local storage works per domain and not by file://...)

--- files/darkmode.js
+++ files/darkmode.js
@@ -1,15 +1,20 @@
 const toggler = document.querySelector(".toggle-input");
 const toggleIcon = document.querySelector(".toggle-icon");
+const webIcons = document.querySelectorAll("#weblink");
+const workIcons = document.querySelectorAll("#workplace");
+const vidIcons = document.querySelectorAll("#demo");
+const pdfIcons = document.querySelectorAll("#pdf");
+const gitIcons = document.querySelectorAll("#gitrepository");
+const uniIcons = document.querySelectorAll("#universityProject");
+const windowsDownloadIcons = document.querySelectorAll("#windowsDownload");
+const personalProjectIcons = document.querySelectorAll("#personalProject");
 const page = document.body;
-console.log("BEFORE CHECK: " + localStorage.getItem("checked"));
+
 setCheckedState();
 
 function setCheckedState(){
     if(!(localStorage.checked === undefined)){
         toggler.checked = isTrue(localStorage.getItem("checked"));
-        console.log(toggler.checked);
-        console.log(localStorage.getItem("checked"));
-        console.log("MID CHECK: " + localStorage.getItem("checked"));
         toggleTheme();
     }
 }
@@ -20,16 +25,12 @@ function toggleTheme() {
     updateLocalStorage();
 }
 
-/*function replaceClass(){
+function replaceClass(){
     if(toggler.checked){
-        console.log("BEFORE: " + page.classList);
         page.classList.replace("light-mode", "dark-mode");
-        console.log("AFTER: " + page.classList);
     }
     else{
-        console.log("BEFORE: " + page.classList);
         page.classList.replace("dark-mode", "light-mode");
-        console.log("AFTER: " + page.classList);
     }
 }
 
@@ -37,10 +38,80 @@ function toggleIconTheme(){
     if(page.classList.contains('light-mode')){
         toggleIcon.src = "./images/moon.png";
         toggleIcon.alt = "Switch to Dark Mode";
+
+        if(webIcons.length !== 0){
+            // Invert icons
+            webIcons.forEach(element => {
+                element.src = "./images/web.png"
+            });
+
+            workIcons.forEach(element => {
+                element.src = "./images/workplace.png"
+            });
+
+            vidIcons.forEach(element => {
+                element.src = "./images/video.png"
+            });
+
+            pdfIcons.forEach(element => {
+                element.src = "./images/report.png"
+            });
+
+            gitIcons.forEach(element => {
+                element.src = "./images/github.png"
+            });
+
+            uniIcons.forEach(element => {
+                element.src = "./images/university.png"
+            });
+
+            windowsDownloadIcons.forEach(element => {
+                element.src = "./images/windows.png"
+            });
+
+            personalProjectIcons.forEach(element => {
+                element.src = "./images/personalProject.png"
+            });
+        }
     }
     else{
         toggleIcon.src = "./images/sun.png";
         toggleIcon.alt = "Switch to Light Mode";
+
+        if(webIcons.length !== 0){
+            // Invert icons
+            webIcons.forEach(element => {
+                element.src = "./images/web-bright.png"
+            });
+
+            workIcons.forEach(element => {
+                element.src = "./images/workplace-bright.png"
+            });
+
+            vidIcons.forEach(element => {
+                element.src = "./images/video-bright.png"
+            });
+
+            pdfIcons.forEach(element => {
+                element.src = "./images/report-bright.png"
+            });
+
+            gitIcons.forEach(element => {
+                element.src = "./images/github-bright.png"
+            });
+
+            uniIcons.forEach(element => {
+                element.src = "./images/university-bright.png"
+            });
+
+            windowsDownloadIcons.forEach(element => {
+                element.src = "./images/windows-bright.png"
+            });
+
+            personalProjectIcons.forEach(element => {
+                element.src = "./images/personalProject-bright.png"
+            });
+        }
     }
 }
 
@@ -52,4 +123,4 @@ function isTrue(value){
     return value === 'true';
 }
 
-toggler.addEventListener("change", toggleTheme);*/
+toggler.addEventListener("change", toggleTheme);
